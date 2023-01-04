cb5a82caafd966d8265d7135a34b187c8b480009
Oussama Saidi <ouss.saidi1@gmail.com>
2023-01-04 08:48:57 +0100

    test

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -7,5 +7,5 @@ from django.db import models
 class Dht11(models.Model):
     id = models.AutoField(primary_key = True)
     temp = models.FloatField(null=False)
-    hum = models.FloatField(null=Flase)
+    hum = models.FloatField(null=False)
     dt = models.DateTimeField(auto_now_add=True,null=True)
