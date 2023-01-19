03f17dd5a13a43f27e79a74b519735bf2b14e3a2
Oussama Saidi <ouss.saidi1@gmail.com>
2023-01-19 13:19:26 +0100

    test

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+from datetime import datetime, timedelta
+
+# Getting Datetime from timestamp
 
 class Dht11(models.Model):
     id = models.AutoField(primary_key = True)
     temp = models.FloatField(null=False)
     hum = models.FloatField(null=False)
-    dt = models.DateTimeField(auto_now_add=True,null=True)
+    dt = models.DateTimeField(null=False)
